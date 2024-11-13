fa01354f5f745f07a3093697ba51f515de679d0b
Daniele Greco <dani.greco1012@gmail.com>
2024-11-13 22:08:58 +0100

    remove scripts folder - deprecated

--- scripts/create_topic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Attende qualche secondo per garantire che Kafka sia pronto
-sleep 10
-# Crea il topic "my-topic" con 1 partizione e fattore di replica 1
-kafka-topics --create --topic my-topic --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1
