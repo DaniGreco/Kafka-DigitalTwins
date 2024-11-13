741fa08daa55c5ddc46985164023cbbf6a051fe4
Daniele Greco <dani.greco1012@gmail.com>
2024-11-13 22:33:15 +0100

    producer update

--- producer/producer.py
+++ producer/producer.py
@@ -5,9 +5,12 @@ producer = KafkaProducer(bootstrap_servers='broker:9092')
 topic = 'my-topic'
 i = 0
 
+print("time to sleep")
+time.sleep(5)
+
 while True:
     i += 1
     message = "Messaggio di prova " + str(i)
     producer.send(topic, message.encode('utf-8'))
-    print("Messaggio inviato" + message)
+    print("Messaggio inviato: " + message)
     time.sleep(1)
