9eb47caf1cc2ac17e6070da121bdc962a236ee44
Matias Negro <m.negro1@studenti.unibg.it>
2025-07-07 17:58:15 +0200

    boh

--- consumer/consumer.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('my-topic', bootstrap_servers='broker:9092')
-
-for message in consumer:
-    print("Messaggio ricevuto: " + message.value.decode('utf-8'))
-
-
